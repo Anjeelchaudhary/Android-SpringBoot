ca1870b0809c9cf03e90e475fca33642d3ed84e9
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-01-27 16:21:21 +0545

    updated buttons and textfield in the

--- Unit_Converter/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Unit_Converter/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private TextView textView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
     }
 }
