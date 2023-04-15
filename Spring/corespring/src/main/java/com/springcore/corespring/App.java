149d19371a080467c2b0496f99bc360f0fce26c7
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-15 06:58:36 +0545

    fixed bugs

--- Spring/corespring/src/main/java/com/springcore/corespring/App.java
+++ Spring/corespring/src/main/java/com/springcore/corespring/App.java
@@ -1,10 +1,7 @@
 package com.springcore.corespring;
 
-import org.springframework.context.*;
-
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    ApplicationContext context =  new ClassPathXmlAppLicationContext("config.xml");
   }
 }
