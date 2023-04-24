54256ab6343dc8c4cd4b2c7fa69b43d0ec7996cb
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-24 06:22:34 +0545

    created calsspathxmlapplication

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/coresprin/App.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/coresprin/App.java
@@ -1,7 +1,13 @@
 package com.corespring.coresprin;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    ApplicationContext context = new ClassPathXmlApplicationContext("configFile.xml");
+    Student student1 = (Student) context.getBean("Student1");
+    System.out.print(student1);
   }
 }
