ca065c490b7ba24177418a4d15ef902773982145
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-24 06:42:27 +0545

    adding second bean

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/coresprin/App.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/coresprin/App.java
@@ -8,6 +8,8 @@ public class App {
     System.out.println("Hello World!");
     ApplicationContext context = new ClassPathXmlApplicationContext("configFile.xml");
     Student student1 = (Student) context.getBean("Student1");
-    System.out.print(student1);
+    Student student2 = (Student) context.getBean("Student2");
+    System.out.println(student1);
+    System.out.println(student2);
   }
 }
