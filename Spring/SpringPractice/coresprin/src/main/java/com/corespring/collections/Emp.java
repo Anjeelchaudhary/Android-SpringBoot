1241d2883f807847df93bdc7f2dc2ff7f04422da
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-24 20:27:15 +0545

    getter & setter added

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/collections/Emp.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/collections/Emp.java
@@ -1,5 +1,47 @@
 package com.corespring.collections;
 
-public class Emp {
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class Emp {
+	private String name;
+	private List<String> phones;
+	private Set<String> addressess;
+	private Map<String , String> courses;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<String> phones) {
+		this.phones = phones;
+	}
+	public Set<String> getAddressess() {
+		return addressess;
+	}
+	public void setAddressess(Set<String> addressess) {
+		this.addressess = addressess;
+	}
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Emp(String name, List<String> phones, Set<String> addressess, Map<String, String> courses) {
+		super();
+		this.name = name;
+		this.phones = phones;
+		this.addressess = addressess;
+		this.courses = courses;
+	} 
 }
