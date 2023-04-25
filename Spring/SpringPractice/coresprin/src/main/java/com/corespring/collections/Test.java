3e50d84f487ed2e51919f63da6fe4b56322fe3dd
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-25 15:06:19 +0545

    fixed bug and added details

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/collections/Test.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/collections/Test.java
@@ -6,9 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/corespring/collection/collectionconfig.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/corespring/collections/collectionconfig.xml");
 		Emp emp1 = (Emp) context.getBean("employee1");
-		System.out.print(emp1.getName());
+		System.out.println(emp1.getName());
+		System.out.println(emp1.getPhones());
+		System.out.println(emp1.getAddressess());
+		System.out.println(emp1.getCourses());
 	}
 
 }
