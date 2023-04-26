c7c6e3325d1d2307ab96a42b5becd128ea9a3c5c
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-26 22:03:05 +0545

    contructor injection

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/contructorref/Person.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/contructorref/Person.java
@@ -1,5 +1,18 @@
 package com.corespring.contructorref;
 
 public class Person {
+	private String personName;
+	private int personId;
+	
+	Person(String personName,int personId) {
+		this.personName = personName;
+		this.personId = personId;
+	}
+
+	@Override
+	public String toString() {
+		return this.personName + " : "+this.personId;
+	}
+	
 	
 }
