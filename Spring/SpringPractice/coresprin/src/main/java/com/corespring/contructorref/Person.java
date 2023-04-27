744c7a41d8325199ce17f16193fe84ef7db12aef
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-27 20:38:22 +0545

    contructor injection with list

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/contructorref/Person.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/contructorref/Person.java
@@ -1,19 +1,23 @@
 package com.corespring.contructorref;
 
+import java.util.List;
+
 public class Person {
 	private String personName;
 	private int personId;
 	private Certificate certify;
+	private List<String> list;
 	
-	Person(String personName,int personId,Certificate certify) {
+	Person(String personName,int personId,Certificate certify,List<String> list) {
 		this.personName = personName;
 		this.personId = personId;
 		this.certify = certify;
+		this.list = list;
 	}
 
 	@Override
 	public String toString() {
-		return this.personName + " : "+this.personId+"{" + this.certify+"}";
+		return this.personName + " : "+this.personId+"{" + this.certify+"}"+this.list;
 	}
 	
 	
