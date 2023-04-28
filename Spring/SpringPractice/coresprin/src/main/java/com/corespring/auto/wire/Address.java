a90855868625a5af1b569df701acb510ee9f855f
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-28 08:44:35 +0545

    getterSetter & toString

--- Spring/SpringPractice/coresprin/src/main/java/com/corespring/auto/wire/Address.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/auto/wire/Address.java
@@ -1,5 +1,27 @@
 package com.corespring.auto.wire;
 
 public class Address {
-
+	private String city;
+	private String wadaNo;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getWadaNo() {
+		return wadaNo;
+	}
+	public void setWadaNo(String wadaNo) {
+		this.wadaNo = wadaNo;
+	}
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", wadaNo=" + wadaNo + "]";
+	}
+	
 }
