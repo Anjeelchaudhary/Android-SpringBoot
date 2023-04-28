14d6f071dd7786a8f74271a50706f3bb2c0e541e
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-28 13:58:10 +0545

    autoWiring with annotation

---
+++ Spring/SpringPractice/coresprin/src/main/java/com/corespring/auto/wire/annotation/Test.java
@@ -0,0 +1,14 @@
+package com.corespring.auto.wire.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/corespring/auto/wire/configFileauto.xml");
+		Emp em = context.getBean(Emp.class);
+		System.out.println(em);
+	}
+
+}
