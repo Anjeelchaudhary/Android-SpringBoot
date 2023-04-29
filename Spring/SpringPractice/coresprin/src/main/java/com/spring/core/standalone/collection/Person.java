2b036dcc13d5a2e9ce4ced847acedcec40751abf
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-29 14:10:00 +0545

    Map and HashMap

--- Spring/SpringPractice/coresprin/src/main/java/com/spring/core/standalone/collection/Person.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/spring/core/standalone/collection/Person.java
@@ -1,9 +1,19 @@
 package com.spring.core.standalone.collection;
 
 import java.util.List;
+import java.util.Map;
 
 public class Person {
 	private List<String> friendsName;
+	private Map<String, Integer> subjects;
+
+	public Map<String, Integer> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Map<String, Integer> subjects) {
+		this.subjects = subjects;
+	}
 
 	public List<String> getFriendsName() {
 		return friendsName;
@@ -15,6 +25,6 @@ public class Person {
 
 	@Override
 	public String toString() {
-		return "Person [friendsName=" + friendsName + "]";
+		return "Person [friendsName=" + friendsName + ", subjects=" + subjects + "]";
 	}
 }
