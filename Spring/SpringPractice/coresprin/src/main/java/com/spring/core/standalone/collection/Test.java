5d61f3b87f52364387603fcdb7deb0995e7c2e8c
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-29 13:27:08 +0545

    collections of List

--- Spring/SpringPractice/coresprin/src/main/java/com/spring/core/standalone/collection/Test.java
+++ Spring/SpringPractice/coresprin/src/main/java/com/spring/core/standalone/collection/Test.java
@@ -1,5 +1,13 @@
 package com.spring.core.standalone.collection;
 
-public class Test {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/core/standalone/collection/aloneconfig.xml");
+		Person p1 =  (Person) context.getBean("persoo1");
+		System.out.println(p1);
+		System.out.println(p1.getFriendsName().getClass().getName());
+	}
 }
