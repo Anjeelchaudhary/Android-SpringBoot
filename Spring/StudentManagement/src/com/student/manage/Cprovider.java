f633305c8a93359dcec7a3237b530cb26c01ab60
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-04-30 10:06:24 +0545

    Created connection and loaded

--- Spring/StudentManagement/src/com/student/manage/Cprovider.java
+++ Spring/StudentManagement/src/com/student/manage/Cprovider.java
@@ -1,12 +1,29 @@
 package com.student.manage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Cprovider {
 	
-	public void create () {
+	static Connection con;
+	
+	public static Connection create () {
 		try {
+			//loading drive
 			Class.forName("com.mysql.jdbc.Driver");
+			
+			//creating connection
+			String user = "anjeel";
+			String password = "password";
+			String url = "jdbc:mysql://localhost:3306/StudentManage"; //<-This is same for all 
+			
+			con = DriverManager.getConnection(url, user, password);  //builtedConnection
+			 
+			
+			
 		}catch(Exception e) {
 			System.out.println(e);
 		}
+		return con;
 	}
 }
---
+++ Spring/StudentManagement/src/com/student/manage/StudentC.java
@@ -0,0 +1,11 @@
+package com.student.manage;
+
+public class StudentC {
+	public static void stuCreate(Student st) {
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
