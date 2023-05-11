629c64e472d62ed095e4cb11cd2b5913dd3759db
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-05-11 10:55:49 +0545

    making newDirectory and creating newJDBC review

---
+++ Spring/jdbc/src/jdbc/Test.java
@@ -0,0 +1,23 @@
+package jdbc;
+
+/* 
+ * JDBC performs in 7 steps
+ * 1:- loading of package
+ * 2:- loading and registering
+ * 3:- connecting
+ * 4:- statement
+ * 5:- query
+ * 6:- execute query
+ * 7:- close
+ * 
+ * */
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
