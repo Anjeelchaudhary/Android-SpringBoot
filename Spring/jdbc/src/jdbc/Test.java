653d0cf23d9c8672bb14f5cd9d660580677c6dad
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-05-13 10:30:29 +0545

    fully processed in jdbc

--- Spring/jdbc/src/jdbc/Test.java
+++ Spring/jdbc/src/jdbc/Test.java
@@ -2,6 +2,8 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /* 
  * JDBC performs in 7 steps
@@ -20,13 +22,19 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		String name = "";
-		String url = "";
-		String query = "";
+		String name = "root";
+		String url = "jdbc:mysql://loaclhost:3306:student";
+		String password = "123456789";
+		String query = "select name from student where id=1";
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(url, name , query);
-
+		Connection con = DriverManager.getConnection(url, name , password);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		String nameOne = rs.getString("name");
+		System.out.println(nameOne);
+		st.close();
+		con.close();
 	}
 
 }
