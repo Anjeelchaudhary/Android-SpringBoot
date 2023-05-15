9a489418deab04fa4362db0b553f70b8aedc4c55
Anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-05-15 07:57:20 +0545

    added PrintWritter servlet

--- Spring/DemoApp/src/main/java/com/servlet/AddServlet.java
+++ Spring/DemoApp/src/main/java/com/servlet/AddServlet.java
@@ -1,15 +1,18 @@
 package com.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void service (HttpServletRequest req, HttpServletResponse res) {
+	public void service (HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int x = Integer.parseInt(req.getParameter("num1"));
 		int y = Integer.parseInt(req.getParameter("num2"));
 		int sum = x + y;
-		System.out.println("hello world");
-		System.out.println(sum);
+		PrintWriter out = res.getWriter();
+		out.println("sum of two number is = "+sum);
 	}
 }
