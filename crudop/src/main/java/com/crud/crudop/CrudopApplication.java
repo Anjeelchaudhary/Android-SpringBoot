0a3748ad83b32c196fe33ba3fe61c84d5c3fd817
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-09 22:42:15 +0545

    added GetMapping and PostMapping

--- crudop/src/main/java/com/crud/crudop/CrudopApplication.java
+++ crudop/src/main/java/com/crud/crudop/CrudopApplication.java
@@ -8,6 +8,7 @@ public class CrudopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudopApplication.class, args);
+		System.out.println("starting");
 	}
 
 }
--- crudop/src/main/java/com/crud/crudop/controller/UserController.java
+++ crudop/src/main/java/com/crud/crudop/controller/UserController.java
@@ -3,9 +3,9 @@ package com.crud.crudop.controller;
 import com.crud.crudop.model.User;
 import com.crud.crudop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -19,5 +19,11 @@ public class UserController {
     //through RequestBody we can inject json file in
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    //Through this mapping we can get data from database
+    @GetMapping("/user")
+    List<User> getAllUser() {
+        return userRepository.findAll();
     }
 }
