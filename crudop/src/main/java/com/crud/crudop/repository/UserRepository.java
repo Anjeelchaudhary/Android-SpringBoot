3a9c4593586dbfcd55bf9136905a6916e967bd77
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-09 20:53:56 +0545

    added database configuration

--- crudop/src/main/java/com/crud/crudop/repository/UserRepository.java
+++ crudop/src/main/java/com/crud/crudop/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.crud.crudop.repository;public interface UserRepository {
+package com.crud.crudop.repository;
+
+import com.crud.crudop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
