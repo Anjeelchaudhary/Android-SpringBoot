7412aa75944b32cd6049f9b981e1810d2511f8a3
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-06-13 19:40:05 +0545

    added Test1 for crude

---
+++ practiceforcrude/src/main/java/com/crudtest/practiceforcrude/PracticeforcrudeApplication.java
@@ -0,0 +1,13 @@
+package com.crudtest.practiceforcrude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeforcrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeforcrudeApplication.class, args);
+	}
+
+}
